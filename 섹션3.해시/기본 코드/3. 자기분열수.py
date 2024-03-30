14cdc4acd8da07357ca16eac8db2f2fa2290a184
joojjang <itchu999@naver.com>
2024-03-30 15:15:07 +0900

    섹션3 3

--- 섹션3.해시/기본 코드/3. 자기분열수.py
+++ 섹션3.해시/기본 코드/3. 자기분열수.py
@@ -1,12 +1,27 @@
 from collections import Counter
-def solution(nums):
-    answer = 0
-    
 
-    return answer 
+def solution(nums):
+    answer = float('inf')
+    frequency = Counter(nums)
+
+    for key in frequency:
+        if key == frequency[key]:
+            answer = min(answer, key)
+
+    if answer == float('inf'): return -1
+    else: return answer 
                           
 print(solution([1, 2, 3, 1, 3, 3, 2, 4]))
 print(solution([1, 2, 3, 3, 3, 2, 4, 5, 5, 5]))
 print(solution([1, 1, 2, 5, 5, 5, 5, 5, 3, 3, 3, 3, 5]))
 print(solution([7, 6, 7, 7, 8, 8, 8, 8, 7, 5, 7, 7, 7, 8, 8]))
 print(solution([11, 12, 5, 5, 3, 11, 7, 12, 15, 12, 12, 11, 12, 12, 7, 8, 12, 11, 12, 7, 12, 5, 15, 20, 15, 12, 15, 12, 15, 14, 12]))
+
+'''
+정답
+2
+1
+-1
+7
+12
+'''
