a8de4a153d823e0b08afc780e3d96dee616e1184
joojjang <itchu999@naver.com>
2024-04-01 02:27:50 +0900

    섹션3 5

--- 섹션3.해시/기본 코드/5. 팰린드롬의 길이.py
+++ 섹션3.해시/기본 코드/5. 팰린드롬의 길이.py
@@ -1,12 +1,28 @@
 from collections import Counter
-def solution(s):
-    answer = 0
 
-    return answer
+def solution(s):
+    freq = Counter(s)
+    odd = 0
+    length = 0
+
+    for key in freq:
+        if freq[key] % 2 != 0:
+            odd += 1 # 검사용
+            length += freq[key] - 1
+        else: 
+            length += freq[key]
+
+    if odd >= 1:
+        length += 1 # 중간에 놓을 문자
+
+    return length
     
                    
-print(solution("abcbbbccaaeee"))
-print(solution("aabbccddee"))
-print(solution("fgfgabtetaaaetytceefcecekefefkccckbsgaafffg"))
-print(solution("aabcefagcefbcabbcc"))
+print(solution("abcbbbccaaeee"), end=" ")
+print(solution("aabbccddee"), end=" ")
+print(solution("fgfgabtetaaaetytceefcecekefefkccckbsgaafffg"), end=" ")
+print(solution("aabcefagcefbcabbcc"), end=" ")
 print(solution("abcbbbccaa"))
+'''
+11 10 41 17 9
+'''
